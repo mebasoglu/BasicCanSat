242e9a7b31acd0b10e01267881320a51a307e3a1
edriyin1 <edriyin@gmail.com>
2020-06-06 00:15:26 +0300

    ADXL345 & BME280 & nRF24L01 Working Code W/O SD

---
+++ TX.ino
@@ -0,0 +1,74 @@
+#include <ADXL345.h>
+#include <Wire.h>
+
+#include <Adafruit_BME280.h>
+#include <Adafruit_Sensor.h>
+
+#include <SPI.h>
+#include <Enrf24.h>
+#include <nRF24L01.h>
+#include <string.h>
+
+#define P2_0 PA8
+#define P2_1 PA4
+#define P2_2 PA0
+
+Enrf24 radio(P2_0, P2_1, P2_2);  // P2.0=CE, P2.1=CSN, P2.2=IRQ
+const uint8_t txaddr[] = { 0xAA, 0xCC, 0xBE, 0xEF, 0x01 };
+
+ADXL345 accelerometer;
+
+float alt;
+float x_acc;
+float y_acc;
+float z_acc;
+
+#define SEALEVELPRESSURE_HPA (1015)
+Adafruit_BME280 bme;
+
+
+void setup() {
+  Serial.begin(9600);
+  //-------------------------------------------------------------------------------
+  SPI.begin();
+  SPI.setDataMode(SPI_MODE0);
+  SPI.setBitOrder(MSBFIRST);
+  radio.begin();
+  Serial.println("Radio started.");
+  radio.setTXaddress((void*)txaddr);
+  //-------------------------------------------------------------------------------
+  if (!accelerometer.begin()) {
+    Serial.println("ADXL345 didn't started.");
+  }
+  //-------------------------------------------------------------------------------
+  if (!bme.begin()) {
+    Serial.println("BME280 didn't started.");
+  }
+  //-------------------------------------------------------------------------------
+
+  //-------------------------------------------------------------------------------
+}
+void loop() {
+
+  Vector norm = accelerometer.readNormalize();
+  alt = bme.readAltitude(SEALEVELPRESSURE_HPA);
+  x_acc = norm.XAxis;
+  y_acc = norm.YAxis;
+  z_acc = norm.ZAxis;
+
+  String SensorData = String(alt) + "," + String(x_acc) + "," + String(y_acc) + "," + String(z_acc);
+  char SensorDataRF[32];
+  SensorData.toCharArray(SensorDataRF, 31);
+  Serial.println("Altitude, X-Acc, Y-Acc, Z-Acc");
+  Serial.println(SensorDataRF);
+  if (radio.print(SensorDataRF)) {
+    Serial.println("Radio transfered data.");
+  }
+  else {
+    Serial.println("Data didn't transferred.");
+  }
+  radio.flush();
+
+  delay(500);
+
+}
